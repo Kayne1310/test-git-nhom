f3d9156f70c231989a4e6c20caea3e633afc428c
PC <khanhdjkl@gmail.com>
2024-05-20 21:41:15 +0700

    Add project files.

---
+++ test git nhom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace test_git_nhom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
